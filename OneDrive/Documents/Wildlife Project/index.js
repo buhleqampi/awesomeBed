4e3cb7f253a77c66204de29cf5a488dd01c74657
buhleqampi <buhle3857@gmail.com>
2022-11-14 13:06:15 +0200

    second commit

---
+++ OneDrive/Documents/Wildlife Project/index.js
@@ -0,0 +1,21 @@
+var elements = document.getElementsByClassName("card");
+var i;
+
+function one() {
+for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "50%";
+}
+}
+
+function two() {
+for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "50%";
+}
+}
+
+
+function four() {
+for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "25%";
+}
+}
